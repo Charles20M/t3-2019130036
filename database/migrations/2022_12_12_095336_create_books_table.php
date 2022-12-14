9e63183e865535310ce06c8985d2ec98ff03f009
charles <bastiancharles198@gmail.com>
2022-12-15 04:34:16 +0700

    update1

--- database/migrations/2022_12_12_095336_create_books_table.php
+++ database/migrations/2022_12_12_095336_create_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePersonalAccessTokensTable extends Migration
+class CreateBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->timestamp('last_used_at')->nullable();
+        Schema::create('books', function (Blueprint $table) {
+            $table->char('id', 13)->primary;
+            $table->string('judul');
+            $table->integer('halaman');
+            $table->string('kategori');
+            $table->string('penerbit');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('books');
     }
 }
